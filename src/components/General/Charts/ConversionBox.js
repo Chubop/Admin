1afa48ea845dac6c79bcf838974630beb53b294c
skirby <skirby@liveperson.com>
2022-04-18 13:56:48 -0400

    created ConversionBox component, used at top & bottom of ResumeAiConversionChart

---
+++ src/components/General/Charts/ConversionBox.js
@@ -0,0 +1,64 @@
+import { makeStyles, Typography } from "@material-ui/core";
+import { colors } from '../../../theme/colors';
+import UpCircleArrow from "../../../assets/icons/UpCircleArrow.svg";
+import { theme } from "../../../theme/muiTheme";
+
+
+const useStyles = makeStyles((theme) => ({
+  topTitle: {
+    textTransform: "uppercase",
+    color: colors.greys.mediumGrey,
+    padding: theme.spacing(1)
+  },
+  topSubtitle: {
+    padding: theme.spacing(1),
+    paddingTop: 0
+  },
+  bottomTitle: {
+    paddingBottom: theme.spacing(0.5),
+    paddingTop: 0,
+    textTransform: "uppercase",
+    textAlign: 'center',
+    color: colors.greys.mediumGrey,
+  },
+  bottomSubtitle: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: colors.greys.mediumGrey
+  }
+}));
+
+export default function ConversionBox (props) {
+
+  const classes = useStyles();
+
+  if(props.top){
+    return (
+      <div style={{padding: 8}}>
+        <Typography className={classes.topTitle}>
+          {props.title}
+        </Typography>
+        <Typography className={classes.topSubtitle}>
+          {props.subtitle}
+        </Typography>
+      </div>
+    );
+  }
+
+  else if(props.bottom){
+    return(
+      <div style={{padding: 8}}>
+        <div style={{textAlign: 'center', marginTop: -theme.spacing(3.5), zIndex: 2, position: 'relative'}}>
+          <img style={{width: '2vw'}} src={UpCircleArrow}/>
+        </div>
+        <Typography className={classes.bottomTitle}>
+          CONVERSION
+        </Typography>
+        <Typography className={classes.bottomSubtitle}>
+          {props.subtitle}
+        </Typography>
+      </div>
+    )
+  }
+
+}
